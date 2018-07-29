721f7bc1cd79e8c967049501ad053c5304838046
Nick Robinson <nrobinson13+github@gmail.com>
2018-07-29 09:24:59 -0400

    Update

---
+++ amazon_kvclpy/kvcl.py
@@ -0,0 +1,110 @@
+import abc
+import tempfile
+import boto3
+from bitstring import BitArray, BitStream
+import skvideo.io
+
+# RecordProcessor base class
+class FrameProcessorBase(object):
+    '''
+    Base class for implementing a record processor. A RecordProcessor processes frames in a stream.
+    Its methods will be called with this pattern:
+
+    - initialize will be called once
+    - process_frame will be called zero or more times
+    - shutdown will be called if this processor loses its lease
+    '''
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def initialize(self, stream_id):
+        '''
+        Called once by a KCLProcess before any calls to process_frame
+
+        :type stream_id: str
+        :param stream_id: The shard id that this processor is going to be working on.
+        '''
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def process_frame(self, frame, checkpointer):
+        '''
+        Called by a KCLProcess with a list of frames to be processed and a checkpointer which accepts sequence numbers
+        from the frames to indicate where in the stream to checkpoint.
+
+        :type frames: list
+        :param frames: A list of frames that are to be processed. A record looks like
+            {"data":"numpy array","streamName":"someKey","fragmentId":"1234567890"} Note that "data" is a base64
+            encoded string. You can use base64.b64decode to decode the data into a string. We currently do not do this decoding for you
+            so as to leave it to your discretion whether you need to decode this particular piece of data.
+
+        :type checkpointer: amazon_kclpy.kcl.Checkpointer
+        :param checkpointer: A checkpointer which accepts a sequence number or no parameters.
+        '''
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def shutdown(self, checkpointer, reason):
+        '''
+        Called by a KCLProcess instance to indicate that this record processor should shutdown. After this is called,
+        there will be no more calls to any other methods of this record processor.
+
+        :type checkpointer: amazon_kclpy.kcl.Checkpointer
+        :param checkpointer: A checkpointer which accepts a sequence number or no parameters.
+
+        :type reason: str
+        :param reason: The reason this record processor is being shutdown, either TERMINATE or ZOMBIE. If ZOMBIE,
+            clients should not checkpoint because there is possibly another record processor which has acquired the lease
+            for this shard. If TERMINATE then checkpointer.checkpoint() should be called to checkpoint at the end of the
+            shard so that this processor will be shutdown and new processor(s) will be created to for the child(ren) of
+            this shard.
+        '''
+        raise NotImplementedError
+
+    def shutdown_requested(self, checkpointer):
+        """
+        Called by a KCLProcess instance to indicate that this record processor is about to be be shutdown.  This gives
+        the record processor a chance to checkpoint, before the lease is terminated.
+
+        :type checkpointer: amazon_kclpy.kcl.Checkpointer
+        :param checkpointer: A checkpointer which accepts a sequence number or no parameters.
+        """
+        pass
+
+    version = 1
+
+class KVCLProcess(object):
+  def __init__(self, frame_processor, stream_name, endpoint):
+    self.processor = frame_processor
+    self.kvs_client = boto3.client('kinesis-video-media', endpoint_url=endpoint)
+    self.stream_name = stream_name
+
+  def run(self):
+    counter = 0
+    response = self.kvs_client.get_media(
+        StreamName=self.stream_name,
+        StartSelector={
+            'StartSelectorType': 'NOW'
+        }
+    )
+
+    iterchunks = response['Payload'].iter_chunks()
+    first_chunk = next(iterchunks)
+    chunk = BitArray(first_chunk)
+
+    for i in iterchunks:
+        a = BitArray(i)
+        pos = a.find('0x1a45dfa3')
+        if pos:
+            chunk.append(a[:pos[0]])
+            f = tempfile.NamedTemporaryFile(delete=False, suffix='.mkv')
+            print(f.name)
+            f.write(chunk.tobytes())
+            videogen = skvideo.io.vreader(f.name, backend="libav")
+            for frame in videogen:
+                if counter % 20 == 1:
+                    self.processor.process_frame(frame)
+            chunk = a[pos[0]:]
+        else:
+            chunk.append(a)
+        counter += 1
--- consumer.py
+++ consumer.py
@@ -7,68 +7,30 @@ import click
 import queue
 import threading
 
-from darkflow.net.build import TFNet
+#from darkflow.net.build import TFNet
 import cv2
+from amazon_kvclpy import kvcl
 
-options = {"model": "cfg/tiny-yolo-voc.cfg", "load": "bin/tiny-yolo-voc.weights", "threshold": 0.3}
-tfnet = TFNet(options)
+class FrameProcessor(kvcl.FrameProcessorBase):
 
-q = queue.Queue()
+    def initialize(self, shard_id):
+        pass
 
-def worker(endpoint, stream):
-    counter = 0
-    client = boto3.client('kinesis-video-media', endpoint_url=endpoint)
+    def process_frame(self, frame, checkpointer):
+        pass
 
-    response = client.get_media(
-        StreamName=stream,
-        StartSelector={
-            'StartSelectorType': 'NOW'
-        }
-    )
-
-    fd, path = tempfile.mkstemp()
-
-    iterchunks = response['Payload'].iter_chunks()
-    first_chunk = next(iterchunks)
-    chunk = BitArray(first_chunk)
-
-    for i in iterchunks:
-        a = BitArray(i)
-        pos = a.find('0x1a45dfa3')
-        if pos:
-            chunk.append(a[:pos[0]])
-            with open('/Volumes/ramdisk/myfile.mkv', 'wb') as w:
-                w.write(chunk.tobytes())
-            videogen = skvideo.io.vreader('/Volumes/ramdisk/myfile.mkv')
-            for frame in videogen:
-                if counter % 20 == 1:
-                    q.put(frame)
-            chunk = a[pos[0]:]
-        else:
-            chunk.append(a)
-        counter += 1
+    def shutdown(self, checkpointer, reason):
+        pass
 
 @click.command()
 @click.option('--endpoint', prompt='Kinesis Video Endpoint', help='Kinesis Video Endpoint.')
 @click.option('--stream', prompt='Kinesis Stream Name', help='Kinesis Stream Name.')
 
 def run(endpoint, stream):
-
-    t = threading.Thread(target=worker, args=(endpoint, stream))
-    t.start()
-
-    while True:
-        item = q.get()
-        if item is None:
-            break
-        #print(q.qsize())
-        mat = cv2.cvtColor(item, cv2.COLOR_BGR2RGB)
-        small_mat = cv2.resize(mat, (0,0), fx=0.4, fy=0.4)
-        result = tfnet.return_predict(small_mat)
-        print(result)
-        #cv2.imshow('image', small_mat)
-        #cv2.waitKey(1)
-        q.task_done()
+    processor = kvcl.KVCLProcess(FrameProcessor(),
+        stream,
+        endpoint)
+    processor.run()
 
 if __name__ == '__main__':
     run()
